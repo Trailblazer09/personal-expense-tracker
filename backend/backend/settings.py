3bd5594de0232d35dcda3fbb5997c0299d23a211
Trailblazer09 <akaaj15o@gmail.com>
2025-08-14 10:08:55 +0530

    Fix render.yaml to use build.sh and correct start command

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m%=06o_oj78j2tksgj+1&#lxoqafv44!k+6*(y#pzrl@c-j@_p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
